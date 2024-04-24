de5d54bd4c598b903d39e4c401249b8551ca9e40
Mohit <its.mohit.kumarr@gmail.com>
2024-04-24 10:59:44 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wave - Chat App",
+  description: "Next-Gen Real-Time Chat App",
+  icons: {
+    icon: [
+      {
+        url: "/images/logo.png",
+        href: "/images/logo.png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
+}
